a21e7644bbcbee289b0213859113a77babda1784
djg5 <djg5@princeton.edu>
2015-01-25 13:27:05 -0500

    fixed the userview to use correct syntax

--- userbase/views.py
+++ userbase/views.py
@@ -8,20 +8,11 @@ def index(request):
     return HttpResponse("Yo, its the homepage")
 
 def user(request, name):
-    print(name)
     user  = User.objects.filter(username=name)[0]
     person = user.person
-    print(user.username)
-    print(person.coins)
     latest_transaction_list = person.transactions.order_by('-date')[:5]
-    actual_list = []
-    for transaction in latest_transaction_list:
-        people = transaction.person_set.all()
-        actual_list.append([people[0].user.username, people[1].user.username, transaction.amount, transaction.date])
-    if (actual_list):
-        print (actual_list)
     context =  {
-        'latest_transaction_list': actual_list,
+        'latest_transaction_list': latest_transaction_list,
         'user': user,
         }
     return render(request, 'userbase/userpage.html', context)
