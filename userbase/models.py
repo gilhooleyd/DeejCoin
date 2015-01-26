ea26e9412f137db747d8cc4c0230e5f48955632a
Lance Goodridge <ldgoodridge95@gmail.com>
2015-01-25 19:59:04 -0500

    Made the Transactions database string more informative

--- userbase/models.py
+++ userbase/models.py
@@ -6,7 +6,13 @@ class Transaction(models.Model):
     amount = models.IntegerField(default=0)
     date = models.DateTimeField('date published')
     def __str__(self):
-        return str(self.amount)
+		if (len(self.person_set.all()) != 2):
+			return str(self.amount)
+		trans_str =  self.person_set.all()[0].user.username + " => "
+		trans_str += self.person_set.all()[1].user.username + ": " 
+		trans_str += str(self.amount)
+		return trans_str
+
 class Person(models.Model):
     user = models.OneToOneField(User)
     coins = models.IntegerField(default=0)
